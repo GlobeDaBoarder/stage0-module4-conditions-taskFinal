91f1772075709f9c3dfa6ae5546393cb332440e7
Gleb Ivashyn <gleb.ivashyn@stud.vgtu.lt>
2022-06-29 18:13:43 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        if(x > 0){
+            if(y > 0){
+                System.out.println("first");
+                return;
+            }
+            System.out.println("second");
+            return;
+        }
+        if (x < 0){
+            if(y > 0){
+                System.out.println("fourth");
+                return;
+            }
+            System.out.println("third");
+            return;
+        }
+
+        System.out.println("zero");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -1,6 +1,21 @@
 package school.mjc.stage0.conditions.finalTask;
 
+import org.w3c.dom.ls.LSOutput;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        if (divider == 0){
+            System.out.println( "division by zero");
+            return;
+        }
+
+        if (dividend == (dividend / divider) * divider){
+            System.out.println("can be divided completely");
+            return;
+        }
+
+        System.out.println("cannot be divided completely");
     }
 }
